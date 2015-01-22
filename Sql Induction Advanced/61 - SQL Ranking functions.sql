3fa6fbc0f2dfb7bf50f0df0a49f8ca14bed18b23
Babu Lal Swami <babulal.swami@optimusinfo.com>
2015-01-22 19:19:13 +0530

    Final commit with all the queries completed.

--- Sql Induction Advanced/61 - SQL Ranking functions.sql
+++ Sql Induction Advanced/61 - SQL Ranking functions.sql
@@ -9,3 +9,10 @@ select  salary, ROW_NUMBER() over (order by salary desc) as row
 	from (select distinct salary from employee)  innertable
 	
 	)outertable where row%2=1;
+
+select top 5 salary from(
+select  salary, DENSE_RANK() over (order by salary desc) as row
+	from (select distinct salary from employee)  innertable
+	
+	)outertable where row%2=1;
+
